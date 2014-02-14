798b5a6c3ce97c5f8701e99156ce63dce962401a
cesine <cesine@yahoo.com>
2014-02-14 15:55:28 -0500

    bringing in tdfp stimulus from @hasflo https://github.com/cesine/OPrime/pull/38

--- ui/tdfp-stimulus.reel/tdfp-stimulus.js
+++ ui/tdfp-stimulus.reel/tdfp-stimulus.js
@@ -2,7 +2,9 @@
  * @module ui/tdfp-stimulus.reel
  * @requires core/contextualizable-component
  */
-var AbstractStimulus = require("oprime-montage/core/abstract-stimulus").AbstractStimulus;
+var ContextualizableComponent = require("oprime-montage/core/contextualizable-component").ContextualizableComponent,
+  AbstractStimulus = require("oprime-montage/core/abstract-stimulus").AbstractStimulus;
+
 
 /**
  * @class TdfpStimulus
@@ -12,8 +14,14 @@ exports.TdfpStimulus = AbstractStimulus.specialize( /** @lends TdfpStimulus# */ 
   constructor: {
     value: function TdfpStimulus() {
       this.super();
+      // this.confirmResponseChoiceMessage = "Es tu sûr?";
+      // this.primeImage = "";
+      // this.audioFile = "";
+      this.canPlayButton1 = true;
+
     }
   },
+
   load: {
     value: function(stimulus) {
 
@@ -27,5 +35,69 @@ exports.TdfpStimulus = AbstractStimulus.specialize( /** @lends TdfpStimulus# */ 
 
       this.super(stimulus);
     }
+  },
+
+
+  spontaneousQuestion1: {
+    value: function() {
+      console.log("Button 1 is pressed ");
+
+      var audioPath = this.audioAssetsPath || "missingpath";
+      audioPath += "/";
+      this.audioFile = audioPath + "TCPP_Paul3.mp3";
+      this.audioElement.src = this.audioFile;
+      this.playAudio();
+      this.canPlayButton2 = true;
+
+    }
+  },
+
+
+  spontaneousQuestion2: {
+    value: function() {
+      console.log("Button 2 is pressed ");
+
+      var audioPath = this.audioAssetsPath || "missingpath";
+      audioPath += "/";
+      this.audioFile = audioPath + "TCPP_Lou3.mp3";
+      this.audioElement.src = this.audioFile;
+
+      this.playAudio();
+      this.canPlayButton1 = false;
+      this.canPlayButton3 = true;
+
+    }
+  },
+
+  delayedImitation: {
+    value: function() {
+      console.log(" Button 3 is pressed ");
+
+      var audioPath = this.audioAssetsPath || "missingpath";
+      audioPath += "/";
+      this.audioFile = audioPath + "TCPP_Lucas3.mp3";
+      this.audioElement.src = this.audioFile;
+
+      this.playAudio();
+      this.canPlayButton2 = false;
+      this.canPlayButton4 = true;
+
+    }
+  },
+
+  immediateImitation: {
+    value: function() {
+      console.log(" Button 4 is pressed ");
+
+      var audioPath = this.audioAssetsPath || "missingpath";
+      audioPath += "/";
+      this.audioFile = audioPath + "TCPP_Guy3.mp3";
+      this.audioElement.src = this.audioFile;
+
+      this.playAudio();
+      this.canPlayButton3 = false;
+
+    }
   }
+
 });
